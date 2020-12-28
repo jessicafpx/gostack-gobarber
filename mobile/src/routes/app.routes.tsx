36ac4bb0e13fbc4840cce46128959cc8dd9d2410
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2020-12-28 17:10:04 -0300

    feat: add routes

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
+
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 
@@ -9,10 +14,16 @@ const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
       headerShown: false,
-      cardStyle: { backgroundColor: '#312e38' },
+      cardStyle: {
+        backgroundColor: '#312e38',
+      },
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
