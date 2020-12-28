576b5f5ba664f93b48ebfa8c0026523072ebfe21
Jessica Peixoto <jessica.fpeixoto@gmail.com>
2020-12-28 17:33:40 -0300

    chore: add button component

--- mobile/src/components/Button/styles.ts
+++ mobile/src/components/Button/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components/native';
 import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled(RectButton)`
-  width: 100%;
+   /* width: 100%; */
   height: 60px;
   background: #ff9000;
   border-radius: 10px;
@@ -16,5 +16,4 @@ export const ButtonText = styled.Text`
   font-family: 'RobotoSlab-Medium';
   color: #312e38;
   font-size: 18px;
-
 `;
